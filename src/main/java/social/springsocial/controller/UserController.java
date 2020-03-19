139a4e0f0dc69bc660b13b4f63838cc24ef1b496
HunHospak <hospak88@gmail.com>
2020-03-19 13:05:49 +0100

    UserController written

--- src/main/java/social/springsocial/controller/UserController.java
+++ src/main/java/social/springsocial/controller/UserController.java
@@ -1,5 +1,25 @@
 package social.springsocial.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import social.springsocial.exception.ResourceNotFoundException;
+import social.springsocial.model.User;
+import social.springsocial.repository.UserRepository;
+import social.springsocial.security.CurrentUser;
+import social.springsocial.security.UserPrincipal;
+
+@RestController
 public class UserController {
 
+  @Autowired
+  private UserRepository userRepository;
+
+  @GetMapping("/user/me")
+  @PreAuthorize("hasRole('USER')")
+  public User getCurrentUser(@CurrentUser UserPrincipal userPrincipal) {
+    return userRepository.findById(userPrincipal.getId())
+        .orElseThrow(() -> new ResourceNotFoundException("User", "id", userPrincipal.getId()));
+  }
 }
