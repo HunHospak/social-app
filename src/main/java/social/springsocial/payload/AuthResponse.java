1dd3a0ef4980f4a2e395f146ea40fc0cffedfba0
HunHospak <hospak88@gmail.com>
2020-03-22 15:53:50 +0100

    added AuthResponse class

--- src/main/java/social/springsocial/payload/AuthResponse.java
+++ src/main/java/social/springsocial/payload/AuthResponse.java
@@ -1,5 +1,26 @@
 package social.springsocial.payload;
 
 public class AuthResponse {
+  private String accesToken;
+  private String tokenType = "Bearer ";
 
+  public AuthResponse(String accesToken) {
+    this.accesToken = accesToken;
+  }
+
+  public String getAccesToken() {
+    return accesToken;
+  }
+
+  public void setAccesToken(String accesToken) {
+    this.accesToken = accesToken;
+  }
+
+  public String getTokenType() {
+    return tokenType;
+  }
+
+  public void setTokenType(String tokenType) {
+    this.tokenType = tokenType;
+  }
 }
