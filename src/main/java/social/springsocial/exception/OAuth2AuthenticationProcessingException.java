65c58c04c51b46c15bed0e8005a1991ae89b9d7a
HunHospak <hospak88@gmail.com>
2020-03-22 16:06:40 +0100

    added exception classes

--- src/main/java/social/springsocial/exception/OAuth2AuthenticationProcessingException.java
+++ src/main/java/social/springsocial/exception/OAuth2AuthenticationProcessingException.java
@@ -1,5 +1,15 @@
 package social.springsocial.exception;
 
-public class OAuth2AuthenticationProcessingException {
+import javax.security.sasl.AuthenticationException;
+
+public class OAuth2AuthenticationProcessingException extends AuthenticationException {
+
+  public OAuth2AuthenticationProcessingException(String msg, Throwable t) {
+    super(msg, t);
+  }
+
+  public OAuth2AuthenticationProcessingException(String msg) {
+    super(msg);
+  }
 
 }
